854ebe78bf0e6bc4d01ed8b9a49247c27cd5f7dd
Estela Medrano <stlmedrano@gmail.com>
2019-05-03 17:47:54 +0200

    add direction in point

--- point.py
+++ point.py
@@ -1,24 +1,43 @@
 class Point:
-    def __init__(self,position):
-        self._check(position[0],position[1])
 
-        self._generate(position)
+    def __init__(self,position,facing):
+        self.CARDINAL_POINTS = ('N','E','S','W')
+
+        self._check_positions(position[0],position[1])
+        self._check_cardinal_point(facing)
+
+        self._generate(position,facing)
 
     def position(self):
-            return [self.eye_x,self.eye_y]
+        return [self.eye_x,self.eye_y]
 
-    def _generate(self,position):
+    def orientation(self):
+        return self.facing        
+
+    def _generate(self,position,facing):
         self.eye_x = position[0]
         self.eye_y = position[1]
+        self.facing = facing
 
-    def _check(self,x,y):
+    def _check_positions(self,x,y):
         if self._is_valid_point(x,y) != True:
             raise Exception('Error in starting point') 
+    
+    def _check_cardinal_point(self,facing):
+        if self._is_invalid_cardinal_point(facing):
+            raise Exception('Error in starting facing') 
+
+    def _is_invalid_cardinal_point(self,facing):  
+        return facing not in self.CARDINAL_POINTS
 
     def _is_valid_point(self,x,y):
         return self._is_valid_ordinate(x) and self._is_valid_ordinate(y)
 
     def _is_valid_ordinate(self,ordinate):
         return (not ordinate is None) and (float(ordinate).is_integer())
+
+    def __str__(self):
+        return "Point x:{} y:{} orientation:{}".format(self.eye_x, self.eye_y,self.facing)
+
 
     
--- test_point.py
+++ test_point.py
@@ -1,31 +1,65 @@
 import unittest 
 from point import Point 
-      
+import re
+
 class TestPoint(unittest.TestCase):  
 
     def test_has_error_in_starting_point(self):
         eye_x = None
         eye_y = 'E'
         start_position = [eye_x,eye_y]
-        facing = ['N']
+        facing = 'N'
 
-        self.assertRaises(Exception, lambda: Point(start_position,facing))
+        init_point = lambda: Point([eye_x,eye_y],facing)
+
+        self.assertRaises(Exception, init_point)
 
     def test_generate_starting_point(self):
         eye_x = 0
         eye_y = 1
         start_position = [eye_x,eye_y]
-        facing = ['N']
+        facing = 'N'
 
         eye_x_objective = 0
         eye_y_objective = 1
         actualy_point = [eye_x_objective,eye_y_objective]
 
-        point = Point(start_position)
+        point = Point(start_position,facing)
 
-        self.assertTrue(point.position(), actualy_point)
+        self.assertEqual(point.position(), actualy_point)
+
+    def test_exception_direction_error(self):
+        eye_x = 0
+        eye_y = 0
+        start_position = [eye_x,eye_y]
+        facing = 'T'
+
+        init_point = lambda: Point(start_position,facing)
+
+        self.assertRaises(Exception, init_point )
 
 
+    def test_west_right_correction(self):
+        eye_x = 0
+        eye_y = 0
+        start_position = [eye_x,eye_y]
+        facing = 'W'
+
+        point = Point(start_position,facing)   
+
+        self.assertEqual(point.orientation(), 'W')
+
+    def test_class_str(self):
+        eye_x = 0
+        eye_y = 0
+        start_position = [eye_x,eye_y]
+        facing = 'W'
+        pattern = '.*{}.*{}.*{}'.format(eye_x,eye_y,facing)
+        
+        STRpoint = Point(start_position,facing) 
+        result = re.match(str(pattern),str(STRpoint))
+
+        self.assertTrue(result)     
    
 
 
